e0934e33fdc15bc794f64bc4db8ad9c1291476c2
Perry Kivolowitz <perry@kivolowitz.com>
2021-03-05 11:27:50 -0600

    allowed descending into hierarchy

--- pull_all.bash
+++ pull_all.bash
@@ -5,7 +5,7 @@
 #	Carthage College Computer Science
 #	Placed in public domain.
 #
-
+me=`pwd`/`basename $0`
 folder='*'
 if [ $# -eq 1 ]
 then
@@ -16,6 +16,7 @@ for d in ${folder}
 do
 	if [ -d ${d} ]; then
 		if [ ${d:0:1} == "_" ]; then
+			# To prevent descending into a directory, prepend its name with an underscore.
 			continue
 		fi
 		if [ -e ${d}/.git ]; then
@@ -28,6 +29,8 @@ do
 				cat /tmp/git.out
 			fi
 			popd > /dev/null 2>&1
+		else
+			${me} ${d}
 		fi
 	fi
 done
