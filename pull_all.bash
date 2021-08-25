d67567e8667667cec06200a7cd5eae7c3bf01ae3
Perry Kivolowitz <perry@kivolowitz.com>
2021-08-25 09:15:22 -0500

    finished making script compatible with nested directories

--- pull_all.bash
+++ pull_all.bash
@@ -5,6 +5,36 @@
 #	Carthage College Computer Science
 #	Placed in public domain.
 #
+
+function Process() {
+	#echo Process called with $@
+	local d
+	for d in $@
+	do
+		if [ -d ${d} ]; then
+			if [ ${d:0:1} == "_" ]; then
+				# To prevent descending into a directory, prepend its name with an underscore.
+				continue
+			fi
+			if [ -e ${d}/.git ]; then
+				pushd $d > /dev/null 2>&1
+				echo $d
+				if git pull | tee /tmp/git.out | grep -q 'Already up to date.'
+				then
+					:
+				else
+					cat /tmp/git.out
+				fi
+				popd > /dev/null 2>&1
+			else
+				pushd ${d} > /dev/null 2>&1
+				Process *
+				popd  > /dev/null 2>&1
+			fi
+		fi
+	done
+}
+
 me=`pwd`/`basename $0`
 folder='*'
 if [ $# -eq 1 ]
@@ -12,26 +42,7 @@ then
 	folder=$1/*
 fi
 
-for d in ${folder}
-do
-	if [ -d ${d} ]; then
-		if [ ${d:0:1} == "_" ]; then
-			# To prevent descending into a directory, prepend its name with an underscore.
-			continue
-		fi
-		if [ -e ${d}/.git ]; then
-			pushd $d > /dev/null 2>&1
-			echo $d
-			if git pull | tee /tmp/git.out | grep -q 'Already up to date.'
-			then
-				:
-			else
-				cat /tmp/git.out
-			fi
-			popd > /dev/null 2>&1
-		else
-			${me} ${d}
-		fi
-	fi
-done
+
+Process ${folder}
+
 rm -f /tmp/git.out
